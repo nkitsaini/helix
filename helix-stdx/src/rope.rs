2574164789c5fb6d20e7641b7ae75a5d451fd1b0
Gabriel Dinner-David <gabydinnerdavid@gmail.com>
2022-11-11 17:48:12 -0500

    toggling of block comments

--- helix-stdx/src/rope.rs
+++ helix-stdx/src/rope.rs
@@ -3,6 +3,8 @@ use ropey::RopeSlice;
 pub trait RopeSliceExt: Sized {
     fn ends_with(self, text: &str) -> bool;
     fn starts_with(self, text: &str) -> bool;
+    fn first_non_whitespace_char(self) -> Option<usize>;
+    fn last_non_whitespace_char(self) -> Option<usize>;
 }
 
 impl RopeSliceExt for RopeSlice<'_> {
@@ -22,5 +24,15 @@ impl RopeSliceExt for RopeSlice<'_> {
         }
         self.get_byte_slice(..len - text.len())
             .map_or(false, |start| start == text)
+    }
+
+    fn first_non_whitespace_char(self) -> Option<usize> {
+        self.chars().position(|ch| !ch.is_whitespace())
+    }
+    fn last_non_whitespace_char(self) -> Option<usize> {
+        self.chars_at(self.len_chars())
+            .reversed()
+            .position(|ch| !ch.is_whitespace())
+            .map(|pos| self.len_chars() - pos - 1)
     }
 }
